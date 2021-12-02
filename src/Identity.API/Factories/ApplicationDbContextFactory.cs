2eb539ac4e93108614cb4ccb34714c53c933ac16
Quang Huy Trần <traquahu1997@gmail.com>
2021-12-02 22:58:59 +0700

    seeding data identity, configuration identity

---
+++ src/Identity.API/Factories/ApplicationDbContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using Identity.API.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Reflection;
+
+namespace Identity.API.Factories
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+               .SetBasePath(Path.Combine(Directory.GetCurrentDirectory()))
+               .AddJsonFile("appsettings.json")
+               .AddEnvironmentVariables()
+               .AddUserSecrets(Assembly.GetAssembly(typeof(Startup)))
+               .Build();
+
+            var optionBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            optionBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"), sqlServerOptionsAction: i => i.MigrationsAssembly(typeof(Startup).Assembly.FullName));
+
+            return new ApplicationDbContext(optionBuilder.Options);
+        }
+
+    }
+}
