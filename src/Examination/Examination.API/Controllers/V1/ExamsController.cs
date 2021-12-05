939688f1ea1c6f5a9ccffd3d76d57b530c3cd737
Quang Huy Trần <traquahu1997@gmail.com>
2021-12-05 23:45:21 +0700

    Intergration Serilog

--- src/Examination/Examination.API/Controllers/V1/ExamsController.cs
+++ src/Examination/Examination.API/Controllers/V1/ExamsController.cs
@@ -1,6 +1,7 @@
 ﻿using Examination.Application.Queries.V1.GetHomeExamList;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 
 namespace Examination.API.Controllers
@@ -11,18 +12,22 @@ namespace Examination.API.Controllers
     public class ExamsController : ControllerBase
     {
         private readonly IMediator _mediator;
+        private readonly ILogger<ExamsController> _logger;
 
-        public ExamsController(IMediator mediator)
+        public ExamsController(IMediator mediator, ILogger<ExamsController> logger)
         {
             _mediator = mediator;
+            _logger = logger;
         }
 
         [HttpGet]
         public async Task<IActionResult> GetExamListAsync()
         {
+            _logger.LogInformation("BEGIN: GetExamList");
             var query = new GetHomeExamListQuery();
             var result = await _mediator.Send(query);
 
+            _logger.LogInformation("END: GetExamList");
             return Ok(result);
         }
     }
