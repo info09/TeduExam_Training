467efbf5036859e0838d29795c2842b3e76e93aa
Quang Huy Trần <traquahu1997@gmail.com>
2021-12-06 22:45:48 +0700

    seeding data mongodb

--- src/Examination/Examination.API/Program.cs
+++ src/Examination/Examination.API/Program.cs
@@ -1,6 +1,12 @@
+using Examination.Infrastructure;
+using Examination.Infrastructure.SeedWork;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
 using Serilog;
 using System;
 using System.IO;
@@ -21,9 +27,18 @@ namespace Examination.API
             {
                 Log.Information("Starting web host ({ApplicationContext})...", appName);
 
-                var host = CreateHostBuilder(args).Build();
+                var host = CreateHostBuilder(configuration, args).Build();
 
                 Log.Information("Apply configuration web host ({ApplicationContext})...", appName);
+
+                using(var scope = host.Services.CreateScope())
+                {
+                    var services = scope.ServiceProvider;
+                    var logger = services.GetRequiredService<ILogger<ExamMongoDbSeeding>>();
+                    var settings = services.GetRequiredService<IOptions<ExamSettings>>();
+                    var mongoClient = services.GetRequiredService<IMongoClient>();
+                    new ExamMongoDbSeeding().SeedAsync(mongoClient, settings, logger).Wait();
+                }
 
                 host.Run();
 
@@ -64,10 +79,13 @@ namespace Examination.API
             }
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        public static IHostBuilder CreateHostBuilder(IConfiguration configuration, string[] args) =>
            Host.CreateDefaultBuilder(args)
                .ConfigureWebHostDefaults(webBuilder =>
                {
+                   webBuilder.CaptureStartupErrors(false);
+                   webBuilder.ConfigureAppConfiguration(app => app.AddConfiguration(configuration));
+
                    webBuilder.UseStartup<Startup>();
                    webBuilder.UseSerilog();
                });
