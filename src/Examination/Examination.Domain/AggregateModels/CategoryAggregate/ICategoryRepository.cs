031467da615b8cdd78844f3dba6b51b87e8fc1f4
HuyTQ <huytq3103@gmail.com>
2022-03-08 10:09:45 +0700

    add infrastructure

--- src/Examination/Examination.Domain/AggregateModels/CategoryAggregate/ICategoryRepository.cs
+++ src/Examination/Examination.Domain/AggregateModels/CategoryAggregate/ICategoryRepository.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Examination.Domain.AggregateModels.CategoryAggregate
+﻿namespace Examination.Domain.AggregateModels.CategoryAggregate
 {
     public interface ICategoryRepository
     {
---
+++ src/Examination/Examination.Infrastructure/Repositories/ExamResultRepository.cs
@@ -0,0 +1,22 @@
+﻿using Examination.Domain.AggregateModels.ExamResultAggregate;
+using Examination.Infrastructure.SeedWork;
+using MediatR;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using System.Threading.Tasks;
+
+namespace Examination.Infrastructure.Repositories
+{
+    public class ExamResultRepository : BaseRepository<ExamResult>, IExamResultRepository
+    {
+        public ExamResultRepository(IMongoClient mongoClient, IClientSessionHandle clientSessionHandle, IOptions<ExamSettings> settings, IMediator mediator, string collection) : base(mongoClient, clientSessionHandle, settings, mediator, collection)
+        {
+        }
+
+        public async Task<ExamResult> GetDetails(string userId, string examId)
+        {
+            var filter = Builders<ExamResult>.Filter.Where(i => i.UserId == userId && i.ExamId == examId);
+            return await Collection.Find(filter).FirstOrDefaultAsync();
+        }
+    }
+}
